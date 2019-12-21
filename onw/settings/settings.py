982ef0bd34ff5c90bc753edea9deee6db10da60a
Lixxmint <lixxmint@gmail.com>
2019-12-21 22:58:34 +0300

    Aplha 0.6

--- onw/settings/settings.py
+++ onw/settings/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
-        'HOST': 'postgres',
+        'HOST': 'pg',
         'PORT': '5432',
     }
 }
@@ -129,9 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_DIR = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- run.sh
+++ run.sh
@@ -3,12 +3,14 @@
 
 case $1 in
   dev)
-    python manage.py runserver --settings=onw.settings.dev
+    python manage.py makemigrations
+    python manage.py migrate
+    python manage.py runserver --settings=onw.settings.dev 0.0.0.0:8000
     ;;
   prod)
     python manage.py collectstatic --noinput
     python manage.py makemigrations
     python manage.py migrate
-    python manage.py runserver --settings=onw.settings.prod
+    python manage.py runserver --settings=onw.settings.prod 0.0.0.0:8000
     ;;
 esac
